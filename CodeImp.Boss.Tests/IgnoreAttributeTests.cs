b432acc309a9d631a1d0c99ac5364bfb9d8b3ad0
Pascal van der Heiden <pascal.van.der.heiden@nearfieldinstruments.com>
2024-09-17 13:24:56 +0200

    Improved testing of the BossIgnore attribute

--- CodeImp.Boss.Tests/IgnoreAttributeTests.cs
+++ CodeImp.Boss.Tests/IgnoreAttributeTests.cs
@@ -2,6 +2,13 @@
 {
     internal class IgnoreAttributeTests : TestBase
     {
+        public class ObjWithAllProperties
+        {
+            public int Age { get; set; } = 3;
+
+            public int IgnoredProperty { get; set; } = 4;
+        }
+
         public class ObjWithIgnoredProperty
         {
             public int Age { get; set; } = 6;
@@ -11,7 +18,7 @@
         }
 
         [Test]
-        public void ObjectWithIgnoredProperty()
+        public void SerializeWithIgnoredProperty()
         {
             ObjWithIgnoredProperty obj = new ObjWithIgnoredProperty();
             obj.Age = 18;
@@ -22,11 +29,38 @@
             AssertStreamIsEqualTo(stream, "10-00-00-00-00-00-00-00-0F-01-01-06-12-00-00-00-01-03-41-67-65");
 
             stream.Seek(0, SeekOrigin.Begin);
+            ObjWithAllProperties? result = BossSerializer.Deserialize<ObjWithAllProperties>(stream);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<ObjWithAllProperties>());
+            Assert.That(result.Age, Is.EqualTo(obj.Age));
+            Assert.That(result.IgnoredProperty, Is.EqualTo(new ObjWithAllProperties().IgnoredProperty));
+        }
+
+        [Test]
+        public void DeserializeWithIgnoredProperty()
+        {
+            ObjWithAllProperties obj = new ObjWithAllProperties();
+            obj.Age = 18;
+            obj.IgnoredProperty = 1200;
+            MemoryStream stream = new MemoryStream();
+            BossSerializer.Serialize(obj, stream);
+
+            AssertStreamIsEqualTo(stream, "16-00-00-00-00-00-00-00-0F-02-01-06-12-00-00-00-02-06-B0-04-00-00-02-03-41-67-65-0F-49-67-6E-6F-72-65-64-50-72-6F-70-65-72-74-79");
+
+            stream.Seek(0, SeekOrigin.Begin);
             ObjWithIgnoredProperty? result = BossSerializer.Deserialize<ObjWithIgnoredProperty>(stream);
             Assert.That(result, Is.Not.Null);
             Assert.That(result, Is.InstanceOf<ObjWithIgnoredProperty>());
             Assert.That(result.Age, Is.EqualTo(obj.Age));
             Assert.That(result.IgnoredProperty, Is.EqualTo(new ObjWithIgnoredProperty().IgnoredProperty));
+        }
+
+        public class ObjWithAllFields
+        {
+            public int Age = 3;
+
+            [BossIgnore]
+            public int IgnoredField = 4;
         }
 
         public class ObjWithIgnoredField
@@ -38,9 +72,28 @@
         }
 
         [Test]
-        public void ObjectWithIgnoredField()
+        public void SerializeWithIgnoredField()
         {
             ObjWithIgnoredField obj = new ObjWithIgnoredField();
+            obj.Age = 18;
+            obj.IgnoredField = 1200;
+            MemoryStream stream = new MemoryStream();
+            BossSerializer.Serialize(obj, stream);
+
+            AssertStreamIsEqualTo(stream, "10-00-00-00-00-00-00-00-0F-01-01-06-12-00-00-00-01-03-41-67-65");
+
+            stream.Seek(0, SeekOrigin.Begin);
+            ObjWithAllFields? result = BossSerializer.Deserialize<ObjWithAllFields>(stream);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<ObjWithAllFields>());
+            Assert.That(result.Age, Is.EqualTo(obj.Age));
+            Assert.That(result.IgnoredField, Is.EqualTo(new ObjWithAllFields().IgnoredField));
+        }
+
+        [Test]
+        public void DeserializeWithIgnoredField()
+        {
+            ObjWithAllFields obj = new ObjWithAllFields();
             obj.Age = 18;
             obj.IgnoredField = 1200;
             MemoryStream stream = new MemoryStream();
